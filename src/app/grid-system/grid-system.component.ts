90ed96e9cfb216557bd212ad8711f12837668e0a
Neeraj Singh Katiyar <nkatiyar@asiapac.globalcsc.net>
2018-10-05 20:45:18 +0530

    nav menu changes

--- src/app/grid-system/grid-system.component.ts
+++ src/app/grid-system/grid-system.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Tile} from '../models/tile';
+
 
 @Component({
   selector: 'app-grid-system',
@@ -6,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./grid-system.component.css']
 })
 export class GridSystemComponent implements OnInit {
+
+  tiles: Tile[] = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
 
   constructor() { }
 
