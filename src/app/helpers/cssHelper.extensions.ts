93f1fab37553f811cc78e5369ba13ba6fa6f88c4
neerajkatiyar <neerajkatiyar25@gmail.com>
2019-01-30 21:26:05 +0530

    fixed add class not working

--- src/app/helpers/cssHelper.extensions.ts
+++ src/app/helpers/cssHelper.extensions.ts
@@ -5,19 +5,13 @@ interface HTMLElement {
     } 
 
 HTMLElement.prototype.addClass = function(cName : string): string {  
-    if(this.className && cName)
     return this.className  = (this.className.indexOf(cName) == -1 ? this.className + " " + cName : this.className).trim();
-    return "";
 } 
 
 HTMLElement.prototype.removeClass = function(cName : string): string {  
-    if(this.className && cName)
     return this.className  = (this.className.indexOf(cName) == -1 ? this.className : this.className.replace(cName,"")).trim();
-    return "";
 } 
 
 HTMLElement.prototype.hasClass = function(cName : string): boolean {  
-    if(this.className && cName)
     return this.className.indexOf(cName) == -1 ? false : true;
-    return false;
 } 
