4e17a31af9a4f700dcd95577a8eeb10f8ba97456
Neeraj Katiyar <nkatiyar@csc.com>
2019-03-06 19:28:28 +0530

    fixed mapping issues

--- Api/SpreadApi/SpreadApi/SpreadCommon/Helper/Mapper.cs
+++ Api/SpreadApi/SpreadApi/SpreadCommon/Helper/Mapper.cs
@@ -8,27 +8,28 @@ namespace SpreadCommon.Helper
 {
     public class BsonMapper
     {
+
         public static BsonDocument Map(BsonDocument source, BsonDocument destination)
         {
+            string dest_obj_key;
             if (source != null && destination != null)
             {
                 foreach (var item in source)
                 {
-                    var dest_obj = destination.Where(x => x.Name.ToLower() == item.Name.ToLower()).Select(x => x.Name).FirstOrDefault();
-                    if ((item != null) && dest_obj != null && item.Value.GetType() == destination[dest_obj].GetType())
+                    dest_obj_key = destination.Where(x => x.Name.ToLower() == item.Name.ToLower()).Select(x => x.Name).FirstOrDefault();
+                    if ((item != null) && dest_obj_key != null && item.Value.GetType() == destination[dest_obj_key].GetType())
                     {
                         if (item.Value.GetType().Equals(typeof(BsonArray)))
                         {
-                            MapBsonArray(item.Value as BsonArray, destination[dest_obj] as BsonArray);
-                            (destination[dest_obj] as BsonArray).RemoveAt(0);
+                            MapBsonArray(item.Value as BsonArray, destination[dest_obj_key] as BsonArray);
                         }
                         else if (item.Value.GetType() == typeof(BsonDocument))
                         {
-                            Map(item.Value as BsonDocument, destination[dest_obj] as BsonDocument);
+                            Map(item.Value as BsonDocument, destination[dest_obj_key] as BsonDocument);
                         }
                         else
                         {
-                            destination[dest_obj] = item.Value;
+                            destination[dest_obj_key] = item.Value;
                         }
                     }
                 }
@@ -38,15 +39,22 @@ namespace SpreadCommon.Helper
 
         private static void MapBsonArray(BsonArray source, BsonValue destination)
         {
+            var iSA = true;
+            BsonDocument _bsonDoc = null;
+            BsonArray _bsonArr = null;
             foreach (var item in source)
             {
                 if (item.GetType() == typeof(BsonArray) && destination.GetType() == typeof(BsonArray) && item.GetType() == destination[0].GetType())
                 {
-                    MapBsonArray(item as BsonArray, (destination as BsonArray)[0]);
+                    _bsonArr = (BsonArray)destination[0].DeepClone();
+                    MapBsonArray(item as BsonArray, _bsonArr);
+                    iSA = false;
                 }
                 else if (item.GetType() == typeof(BsonDocument) && destination[0].GetType() == typeof(BsonDocument))
                 {
-                    Map(item as BsonDocument, destination as BsonDocument);
+                    _bsonDoc = (BsonDocument)destination[0].DeepClone();
+                    Map(item as BsonDocument, _bsonDoc);
+                    iSA = false;
                 }
                 else if (item.GetType() == destination.GetType())
                 {
@@ -54,13 +62,28 @@ namespace SpreadCommon.Helper
                 }
                 if (item != null)
                 {
-                    (destination as BsonArray).Add(item);
+                    if (_bsonDoc != null)
+                    {
+                        (destination as BsonArray).Add(_bsonDoc);
+                    }
+                    else if (_bsonArr != null)
+                    {
+                        (destination as BsonArray).Add(_bsonArr);
+                    }
+                    else
+                    {
+                        (destination as BsonArray).Add(item);
+                    }
                 }
+            }
+            if (!iSA)
+            {
+                (destination as BsonArray).RemoveAt(0);
             }
         }
     }
 
-    public class BsonKeyComparer : IEqualityComparer<BsonElement>
+    internal class BsonKeyComparer : IEqualityComparer<BsonElement>
     {
         public bool Equals(BsonElement x, BsonElement y)
         {
